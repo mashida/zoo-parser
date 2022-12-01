af6e38bc5f80c4a72c68f0c4e47e124448eedbd8
Nikita Serditov <serditov.na@gmail.com>
2022-12-01 10:35:11 +0300

    removed tracemalloc usage

--- lib/parser.py
+++ lib/parser.py
@@ -8,7 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 from loguru import logger
 from requests.adapters import HTTPAdapter
-import tracemalloc
 from urllib3 import Retry
 
 from lib.category import Category, STAGES
@@ -177,23 +176,20 @@ class Parser:
                 continue
             index = f'{index}/{len(self.products_list_by_category[catalog_url])}'
             product.parse(articles=self.parsed_articles_list, barcodes=self.parsed_barcodes_list, index=index)
-            logger.info(tracemalloc.get_traced_memory())
         logger.info(f'        Done | Products from {catalog_url} have been parsed')
 
     def parse_cards(self, catalog_url):
         logger.info(f'      Parsing cards out of {catalog_url}')
-        tracemalloc.start()
         soup = self.get_soup_out_of_page_with_url(ZOO_URL + catalog_url, params={'pc': 50, 'v': 'filling'})
         self.calc_amount_of_pages(soup=soup, catalog_url=catalog_url)
         # let's get links of all products in this category
         self.get_all_products_links_out_of_category(catalog_url=catalog_url)
         # let's parse all products we have within this category
         self.parse_all_products_out_of_category(catalog_url=catalog_url)
-        tracemalloc.stop()
 
     def csv_write(self):
+        self.out_dir.mkdir(exist_ok=True)
         # write categories
-
         with (self.out_dir / 'categories.csv').open('w', encoding='utf-8', newline='') as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(headers)
@@ -228,7 +224,7 @@ class Parser:
         for url in self.required_categories_list:
             logger.info(f'  Parsing {url} category')
             self.parse_all_categories(url)
-            # self.parse_cards(url)
+            self.parse_cards(url)
             logger.info(f'  Done | Category {url} parsed')
 
 
